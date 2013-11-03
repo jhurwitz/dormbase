b7c0f69a3e568e96e057b020cd226f5b510aef36
Jacob Hurwitz <jhurwitz@mit.edu>
2013-11-02 21:59:28 -0400

    simplify Package model
    I'll add more fields later, but for now, I wanted to eliminate all the cruft.

--- package/models.py
+++ package/models.py
@@ -18,37 +18,18 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.db import models
-from django.contrib.auth.models import User as AuthUser
 from django import forms
 from residents.models import Resident
 
 class Package(models.Model):
-    BINS = (('A', 'A'),
-            ('B', 'B'),
-            ('C', 'C'),
-            ('D', 'D'),
-            ('F', 'Floor'))
-
     recipient = models.ForeignKey(Resident, null = True)
-    location = models.CharField(max_length=8, choices=BINS)
-    perishable = models.BooleanField()
+    location = models.CharField(max_length=8)
     date_recieved = models.DateField(auto_now_add = True)
-    hidden = models.BooleanField(default = False)
-    #checked_in_by = models.ForeignKey(AuthUser, null = True, blank = True) # Set to true for development
-    #checked_out_by = models.ForeignKey(AuthUser, null = True, blank = True) # Set to true for development
 
     def __unicode__(self):
         return unicode(self.recipient)
 
 class PackageForm(forms.ModelForm):
-    BINS = (('A', 'A'),
-            ('B', 'B'),
-            ('C', 'C'),
-            ('D', 'D'),
-            ('F', 'Floor'))
-
-    location = forms.ChoiceField(choices=BINS)
-
     class Meta:
         model = Package
-        exclude = ('date_received', 'hidden')
+        exclude = ('date_received', )
--- package/views.py
+++ package/views.py
@@ -28,11 +28,10 @@ def package_get(request):
         raise Http404
 
     packages = []
-    for p in Package.objects.filter(hidden=False):
+    for p in Package.objects.all():
         packages.append({
                 'recipient': p.recipient.getFullName(),
                 'location': p.location,
-                'perishable': p.perishable,
                 'id': p.id,
                 })
 
@@ -43,26 +42,23 @@ def package_add(request):
         form = PackageForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            p = Package(recipient = cd['recipient'], 
-                        location = cd['location'], 
-                        perishable = cd['perishable'],)
+            p = Package(recipient = cd['recipient'],
+                        location = cd['location'],)
             p.save()
 
             jsonPackage = json.dumps({'recipient': p.recipient.getFullName(),
                                       'location': p.location,
-                                      'perishable': p.perishable,
                                       'id': p.id})
 
             return HttpResponse(jsonPackage, mimetype="application/json")
 
     raise Http404
-            
+
 def package_remove(request):
     if request.method == 'POST':
         p_id = request.POST['package_id']
         p = Package.objects.get(id = p_id)
-        p.hidden = True
-        p.save()
+        # TODO implement this
         pid = 'p' + str(p.id)
         jsonResponse = json.dumps({'id': pid})
         return HttpResponse(jsonResponse, mimetype="application/json")
