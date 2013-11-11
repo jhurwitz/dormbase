393f654fd4a2f003da10ba3a4d0abb9e74c34e64
Jacob Hurwitz <jhurwitz@mit.edu>
2013-11-11 12:56:31 -0500

    upgrade to the latest Django, v. 1.6
    Dormbase was previously using Django 1.4, and it wasn't too hard to switch.
    (The main problem was a difference in the url function in views, but a quick
    sed fixed that.)
    
    I'm not entirely sure what the benefit of upgrading is because I'm new to
    Django, but I think it's better to upgrade now (before we have a giant
    codebase) than later.

--- manage.py
+++ manage.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
-
-import dormbase.settings as settings
+import os
+import sys
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dormbase.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
